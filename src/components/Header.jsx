379f6c9dab86d44b2a764cef49f6009e263172f5
Utkarsh <utkarshtiwari.1121@gmail.com>
2024-09-30 14:21:19 +0530

    commitagian

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,16 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { NavLink } from "react-router-dom";
 import { Link } from "react-scroll";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Header() {
   const [active, setActive] = useState();
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setActive();
+    setIsOpen(false);
   };
 
   return (
@@ -18,15 +22,26 @@ export default function Header() {
           <DataObjectIcon fontSize="medium" />
         </span>
 
+        <div className="md:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className=" absolute top-0 right-0 text-white flex p-4 fixed"
+          >
+            {isOpen ? <CloseIcon /> : <MenuIcon />}
+          </button>
+        </div>
+
         <motion.div
+          className={`${
+            isOpen ? "flex flex-col bg-gray-800  mr-7 ml-64 mt-12 rounded-lg" : "hidden"
+          } md:flex md:flex-row md:absolute top-0 right-0 md:text-white md:p-4 `}
           whileTap={{ scale: 1.01 }}
-          // drag="x"
-          className="text-white text-right p-4"
+          // className="text-white text-right p-4"
         >
           <Link
-            className={`${
+            className={`${ 
               active === "/" ? "text-red-600" : "text-white"
-            } md:mx-5 mx-2 hover:text-red-600`}
+            } md:mx-5 mx-2  p-2 hover:text-red-600`}
             to="/"
             spy={true}
             smooth={true}
@@ -39,7 +54,7 @@ export default function Header() {
           <Link
             className={`${
               active === "/about" ? "text-red-600" : "text-white"
-            } md:mx-5 mx-2 hover:text-red-600`}
+            } md:mx-5 mx-2  p-2 hover:text-red-600`}
             to="/about"
             spy={true}
             smooth={true}
@@ -52,7 +67,7 @@ export default function Header() {
           <Link
             className={`${
               active === "/project" ? "text-red-600" : "text-white"
-            } md:mx-5 mx-2 hover:text-red-600`}
+            } md:mx-5 mx-2  p-2 hover:text-red-600`}
             to="/project"
             spy={true}
             smooth={true}
@@ -65,7 +80,7 @@ export default function Header() {
           <Link
             className={`${
               active === "/contact" ? "text-red-600" : "text-white"
-            } md:mx-5 mx-2 hover:text-red-600`}
+            } md:mx-5 mx-2  p-2 hover:text-red-600`}
             to="/contact"
             spy={true}
             smooth={true}
